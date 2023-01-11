810160fb52a392bb01c39059cbc95b7416f1b8e5
fayco226 <faicallengane55@gmail.com>
2023-01-11 15:33:33 +0000

    save

--- WEEK_8 - Django_Framwork(1)/Day-5/Mini Project/rent/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from rent.models import Rental
-class RentalAddForm(ModelForm):
-    class meta:
-        model : Rental
-        fields: "__all__"
--- WEEK_8 - Django_Framwork(1)/Day-5/Mini Project/rent/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from rent import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name="home")
-]
